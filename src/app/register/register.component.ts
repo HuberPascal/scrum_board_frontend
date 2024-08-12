ee8225f6a255e735d2e8a830cf1d55cbd131e006
Pascal Huber <pascal.huber92@gmx.ch>
2024-08-12 20:56:28 +0200

    Implement registration form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    CommonModule,
+    RouterLink,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -14,7 +24,14 @@ export class RegisterComponent {
   lastName: string = '';
   username: string = '';
   email: string = '';
-  password: string = '';
+  password1: string = '';
+  password2: string = '';
+  isFirstNameFocused = false;
+  isLastNameFocused = false;
+  isUsernameFocused = false;
+  isEmailFocused = false;
+  isPasswordFocused1 = false;
+  isPasswordFocused2 = false;
 
   constructor(private authService: AuthService) {}
 
@@ -25,12 +42,60 @@ export class RegisterComponent {
         this.lastName,
         this.username,
         this.email,
-        this.password
+        this.password1
       );
       console.log(response);
       // redirect
     } catch (error) {
       console.error(error);
     }
+  }
+
+  onFirstNameFocus() {
+    this.isFirstNameFocused = true;
+  }
+
+  onFirstNameBlur() {
+    this.isFirstNameFocused = false;
+  }
+
+  onLastNameFocus() {
+    this.isLastNameFocused = true;
+  }
+
+  onLastNameBlur() {
+    this.isLastNameFocused = false;
+  }
+
+  onUsernameFocus() {
+    this.isUsernameFocused = true;
+  }
+
+  onUsernameBlur() {
+    this.isUsernameFocused = false;
+  }
+
+  onEmailFocus() {
+    this.isEmailFocused = true;
+  }
+
+  onEmailBlur() {
+    this.isEmailFocused = false;
+  }
+
+  onPasswordFocus1() {
+    this.isPasswordFocused1 = true;
+  }
+
+  onPasswordBlur1() {
+    this.isPasswordFocused1 = false;
+  }
+
+  onPasswordFocus2() {
+    this.isPasswordFocused2 = true;
+  }
+
+  onPasswordBlur2() {
+    this.isPasswordFocused2 = false;
   }
 }
